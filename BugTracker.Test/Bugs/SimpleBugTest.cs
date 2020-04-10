bc97c5f269cab13ca21de44b56feea4c4fe30127
SPGIO <SPGIO@users.noreply.github.com>
2020-04-10 08:57:52 +0200

    Restructoring Bug test

--- BugTracker.Test/Bugs/SimpleBugTest.cs
+++ BugTracker.Test/Bugs/SimpleBugTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BugTracker.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class SimpleBugTest
     {
         [TestMethod]
         public void TestMethod1()
